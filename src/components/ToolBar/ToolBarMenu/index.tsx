4911cfb4daff12bd30a1cda0d2df0b0161709dc9
SNinjo <70991369+SNinjo@users.noreply.github.com>
2022-09-06 08:01:01 +0800

    Revert "update 2022/9/6"
    This reverts commit 4d557b6fd453b4820a440f4e4efa238674b598a0.

--- src/components/ToolBar/ToolBarMenu/index.tsx
+++ src/components/ToolBar/ToolBarMenu/index.tsx
@@ -11,7 +11,6 @@ const ToolBarMenu = memo((props: {toolBarMenu: Menu, isShow: boolean, closeToolB
 
 
     const clickToolBarNode = (event: React.MouseEvent<HTMLLIElement, MouseEvent>, toolBarNode: Node) => {
-        if (toolBarNode.isDisabled()) return;
         event.stopPropagation();
 
         toolBarNode.callClickFunction();
@@ -23,7 +22,6 @@ const ToolBarMenu = memo((props: {toolBarMenu: Menu, isShow: boolean, closeToolB
         let tagNode: JSX.Element = (
             <li
                 key={"ToolBarNode" + index}
-                className={(toolBarNode.isDisabled()? style.disabled : undefined)}
                 onClick={(event) => clickToolBarNode(event, toolBarNode)}
                 data-testid={toolBarNode.setTestId()}
             >
